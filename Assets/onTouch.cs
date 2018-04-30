503d0803ae4c695ceb72289865db2342c04d48dc
Staffan Sandberg <staffan.sandberg94@gmail.com>
2018-04-30 18:59:14 +0200

    Strings with vibration

---
+++ Assets/onTouch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onTouch : MonoBehaviour
+{
+  public int[] vibLength = new int[6];
+  private string[] stringNames = new string[] { "E", "A", "D", "G", "B", "e" };
+  private Dictionary<string, int> stringVib;
+
+  // Use this for initialization
+  void Start()
+  {
+    stringVib = new Dictionary<string, int>();
+    for (int i = 0; i < stringNames.Length; i++)
+    {
+      stringVib.Add(stringNames[i], vibLength[i]);
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Vector2 touchPos = Input.GetTouch(0).position;
+    Ray ray = Camera.main.ScreenPointToRay(touchPos);
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit, 100))
+    {
+      string name = hit.transform.gameObject.name;
+      PlayString(name);
+    }
+  }
+
+  private void PlayString(string name)
+  {
+    Vibration.CreateOneShot(stringVib[name]);
+  }
+}
