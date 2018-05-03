731132a80eff1af998e2d8d22d44e41132d31aac
Staffan <staffan.sandberg94@gmail.com>
2018-05-03 18:28:10 +0200

    Hej

--- Assets/Shred.cs
+++ Assets/Shred.cs
@@ -25,32 +25,3 @@ public class Shred : MonoBehaviour {
 		}
 	}
 }
-
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Shred : MonoBehaviour {
-
-	int count;
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.touchCount > 0 || Input.GetMouseButton(0)) {
-			// Vector2 touchPos = Input.GetTouch(0).position;
-			Vector2 touchPos = Input.mousePosition;
-
-			Ray ray = Camera.main.ScreenPointToRay(touchPos);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit, 10000))
-			{
-				// string name = hit.transform.gameObject.name;
-				Debug.Log("Hej: "+ ++count);
-			}
-		}
-	}
-}
